8a78d71e3f756d8c563b7894cea061fe3dcbd9b8
vplauzon <vincent_lauzon@yahoo.com.au>
2018-06-23 06:49:45 -0400

    .

---
+++ ListBlobs/ListBlobsConsole/build-container.sh
@@ -0,0 +1,8 @@
+﻿#	Build docker container
+sudo docker build -t vplauzon/list-blobs .
+
+#	Publish image
+sudo docker push vplauzon/list-blobs
+
+#	Test image
+#sudo docker run -it vplauzon/list-blobs
