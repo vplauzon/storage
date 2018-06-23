f6b06c0bf38e1e5dc295c46f2d346c4ccbe49854
Vincent-Philippe Lauzon <vilauzon@microsoft.com>
2018-06-23 06:37:38 -0400

    Complete ListBlobs

--- ListBlobs/ListBlobsConsole/Program.cs
+++ ListBlobs/ListBlobsConsole/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.WindowsAzure.Storage.Auth;
 using Microsoft.WindowsAzure.Storage.Blob;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace ListBlobsConsole
@@ -9,24 +10,47 @@ namespace ListBlobsConsole
     {
         static void Main(string[] args)
         {
-            var blobDir = new Uri(Environment.GetEnvironmentVariable("BLOB-DIR"));
+            var blobDir = Environment.GetEnvironmentVariable("BLOB-DIR");
             var sasToken = Environment.GetEnvironmentVariable("SAS-TOKEN");
 
-            MainAsync(blobDir, sasToken).Wait();
+            Console.WriteLine($"BLOB-DIR:  {blobDir}");
+            Console.WriteLine($"SAS-TOKEN:  {sasToken}");
+            Console.WriteLine();
+
+            if (string.IsNullOrWhiteSpace(blobDir))
+            {
+                Console.WriteLine("Missing BLOB-DIR environment variable");
+            }
+            else if (blobDir.Split('/').Length < 4)
+            {
+                Console.WriteLine("BLOB-DIR doesn't point to at least a container");
+            }
+            else if (string.IsNullOrWhiteSpace(sasToken))
+            {
+                Console.WriteLine("Missing SAS-TOKEN environment variable");
+            }
+            else
+            {
+                var split = blobDir.Split('/');
+                var containerUri = new Uri(string.Join('/', split.Take(4)));
+                var folderPath = string.Join('/', split.Skip(4));
+
+                MainAsync(containerUri, folderPath, sasToken).Wait();
+            }
         }
 
-        private static async Task MainAsync(Uri blobDir, string sasToken)
+        private static async Task MainAsync(Uri containerUri, string folderPath, string sasToken)
         {
             var storageCreds = new StorageCredentials(sasToken);
-            var container = new CloudBlobContainer(blobDir, storageCreds);
+            var container = new CloudBlobContainer(containerUri, storageCreds);
+            var folder = container.GetDirectoryReference(folderPath);
             BlobContinuationToken continuationToken = null;
 
             do
             {
-                var segment = await container.ListBlobsSegmentedAsync(
-                    "Partitioned_xmlFiles/",
+                var segment = await folder.ListBlobsSegmentedAsync(
                     true,
-                    BlobListingDetails.Metadata,
+                    BlobListingDetails.None,
                     null,
                     continuationToken,
                     null,
